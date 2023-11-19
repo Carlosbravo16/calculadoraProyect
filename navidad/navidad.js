07fda632a87147f61e08d91850160c5f8909e2b0
Carlosbravo16 <110642356+Carlosbravo16@users.noreply.github.com>
2023-11-18 20:51:31 -0500

    se termina el proyecto de navidad

--- navidad/navidad.js
+++ navidad/navidad.js
@@ -1,17 +1,47 @@
-let di = hr = mn = sg = ms = "0" + 0, iniciarTiempo;
+let papanoel = "off"
+let noelplay = document.getElementById("play");
+let noelpause = document.getElementById("pause");
+let noelStop = document.getElementById("baileOff");
+let botonAudio = new Audio('./sound/audio_navidad.mp3');
 
-const botonIniciar =document.querySelector(".iniciar");
-const botonDetener =document.querySelector(".detener");
+
+botonAudio.disabled = true; 
+            
+
+function bailar(){
+    if(papanoel =="off") {
+        noelplay.addEventListener('click',()=>{
+            botonAudio.play();
+            botonAudio.disabled = true;
+        })
+    } 
+}
+
+    
+    
+function nobailar() {
+        noelpause.addEventListener('click',()=>{
+            botonAudio.pause();
+            botonAudio.disabled = false;
+
+
+    })
+    }
 
 
 
-function obtenerTiempoFaltante(fechaLimite) {
+function obtenerTiempoFaltante(fechaLimite){
     let ahora = new Date();
-    tiempoFaltante = (new Date(fechaLimite) - ahora + 100) / 1000;
-    segundosFaltantes = ('0' + Math.floor(tiempoFaltante % 60)).slice(-2);
-    minutosFaltantes = ('0' + Math.floor(tiempoFaltante / 60 % 60)).slice(-2);
-    horasFaltantes = ('0' + Math.floor(tiempoFaltante / 3600 % 24)).slice(-2);
-    diasFaltantes = ('0' + Math.floor(tiempoFaltante / (3600 * 24))).slice(-2);
+    
+    let tiempoFaltante = (new Date(fechaLimite) - ahora + 1000) / 1000;
+
+    let segundosFaltantes = ('0' + Math.floor(tiempoFaltante % 60)).slice(-2);
+
+    let minutosFaltantes = ('0' + Math.floor(tiempoFaltante / 60 % 60)).slice(-2);
+
+    let horasFaltantes = ('0' + Math.floor(tiempoFaltante / 3600 % 24)).slice(-2);
+
+    let diasFaltantes = ('0' + Math.floor(tiempoFaltante / ( 3600 * 24))).slice(-2);
 
     return{
         segundosFaltantes,
@@ -22,23 +52,34 @@ function obtenerTiempoFaltante(fechaLimite) {
     }
 };
 
-//console.log(obtenerTiempoFaltante('Dec 25 2023 00:00:00 GMT-0500'));
-
-function cuentaRegresiva(tiempoFaltante,reloj,mensaje){
-    const e = document.getElementById(reloj);
+function cuentaRegresiva(tiempoFaltante,mensaje){
+    const titulo = document.getElementById("titulo");
+    const dias = document.getElementById("dias");
+    const horas = document.getElementById("horas");
+    const minutos = document.getElementById("minutos");
+    const segundos = document.getElementById("segundos");
 
     const tiempoActual = setInterval( () => {
         let t = obtenerTiempoFaltante(tiempoFaltante);
-        e.innerHTML = `${t.diasFaltantes}d:${t.horasFaltantes}h:${t.minutosFaltantes}m:${t.segundosFaltantes}s`;
+        dias.innerHTML = t.diasFaltantes;
+        horas.innerHTML = t.horasFaltantes;
+        minutos.innerHTML = t.minutosFaltantes;
+        segundos.innerHTML = t.segundosFaltantes;
 
         if(t.tiempoFaltante < 1){
             clearInterval(tiempoActual);
-            e.innerHTML = mensaje;
+            const tituloa ="";
+            titulo.innerHTML = mensaje;
+            tituloantes.innerHTML = tituloa;
+            dias.innerHTML = '00';
+            horas.innerHTML = '00';
+            minutos.innerHTML = '00';
+            segundos.innerHTML = '00';
+            noelStop.classList.add("on");
         }
     }, 1000)
-};
+}
 
+//console.log(obtenerTiempoFaltante('Dec 25 2023 00:00:00 GMT-0500'))//
 
-cuentaRegresiva('Nov 25 2023 19:32:00 GMT-0500', 'cuentaRegresiva', '¡Feliz Navidad!');
-
-
+cuentaRegresiva('Nov 18 2023 20:51:00 GMT-0500','¡Feliz Navidad!')
